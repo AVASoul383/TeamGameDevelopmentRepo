eaef25dd23d71eb5f2c055ae2c88e9171aba5041
QuantumDev-1 <travis.byers96@gmail.com>
2025-04-20 21:27:47 -0500

    Added debug ray to player camera, raycast in playerInteract script wasn't working.

--- Unity Project/My project (1)/Assets/Scripts/playerInteract.cs
+++ Unity Project/My project (1)/Assets/Scripts/playerInteract.cs
@@ -20,6 +20,7 @@ public class playerInteract : MonoBehaviour
 
     void handleInteraction()
     {
+        Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.forward * interactDistance, Color.red);
         RaycastHit hit;
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactDistance, interactableLayer))
         {
